335a1ee4334df702303765f1997cecd1ef176c23
Doug Harper <doug@zewkey.com>
2015-11-24 16:02:38 -0600

    TableView Titles
    Added tableView protocols and displayed title

--- YouTubeApp/ViewController.swift
+++ YouTubeApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var videos: [Video] = [Video]()
@@ -17,11 +17,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.videos = VideoModel().getVideos()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //: MARK Tableview Protocols
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell")!
+        
+        let videoTitle = videos[indexPath.row].videoTitle
+        
+        // Customize our cell to display the video title
+        cell.textLabel?.text = videoTitle
+        
+        return cell
     }
 
 
